1ddc24b9ff3f5dcb6c5958480b76421576e0744f
Jeremy PERROUAULT <jeremy.perrouault@ascadis.fr>
2017-08-25 14:22:51 +0200

    Auto stash before merge of "master" and "origin/master"

---
+++ src/com/sopra/model/Bloc.java
@@ -0,0 +1,64 @@
+package com.sopra.model;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="bloc")
+public class Bloc
+{
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	@Column(name="BLO_ID")
+	private int id;
+	
+	@Column(name="BLO_X")
+	private int x;
+	
+	@Column(name="BLO_Y")
+	private int y;
+	
+	@ManyToOne
+	@JoinColumn(name="BLO_FIGURE_ID")
+	private Figure figure;
+	
+	
+	
+	public int getId() {
+		return id;
+	}
+	
+	public void setId(int id) {
+		this.id = id;
+	}
+	
+	public int getX() {
+		return x;
+	}
+	
+	public void setX(int x) {
+		this.x = x;
+	}
+	
+	public int getY() {
+		return y;
+	}
+	
+	public void setY(int y) {
+		this.y = y;
+	}
+	
+	public Figure getFigure() {
+		return figure;
+	}
+	
+	public void setFigure(Figure figure) {
+		this.figure = figure;
+	}
+}
--- src/com/sopra/model/Figure.java
+++ src/com/sopra/model/Figure.java
@@ -1,5 +1,7 @@
 package com.sopra.model;
 
+import java.util.List;
+
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
@@ -7,6 +9,7 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
 import javax.persistence.Table;
 import javax.validation.constraints.NotNull;
 
@@ -26,6 +29,11 @@ public class Figure {
 	@ManyToOne
 	@JoinColumn(name="FIG_TETRIMINO_ID")
 	private Tetrimino terimino;
+	
+	@OneToMany(mappedBy="figure")
+	private List<Bloc> blocs;
+	
+	
 
 	public Tetrimino getTerimino() {
 		return terimino;
@@ -51,5 +59,11 @@ public class Figure {
 		this.ordre = ordre;
 	}
 	
+	public List<Bloc> getBlocs() {
+		return blocs;
+	}
 	
+	public void setBlocs(List<Bloc> blocs) {
+		this.blocs = blocs;
+	}
 }
