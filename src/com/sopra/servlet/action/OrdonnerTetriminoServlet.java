045310500fb6b5bd47532e71d75a2b030aaaf6a6
Unknown <gonzague.ducrocq@soprasteria.com>
2017-08-28 14:23:58 +0200

    OrdonnerServlet

---
+++ src/com/sopra/servlet/action/OrdonnerTetriminoServlet.java
@@ -0,0 +1,59 @@
+package com.sopra.servlet.action;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
+import com.sopra.dao.IFigureDAO;
+import com.sopra.dao.ITetriminoDAO;
+import com.sopra.model.Figure;
+import com.sopra.model.Tetrimino;
+
+@WebServlet("ordonnerFigure")
+public class OrdonnerTetriminoServlet extends SpringServlet{
+
+	private static final String VUE_GET					= "/tetrimino/admin/modifPiece";
+
+	@Autowired
+	private IFigureDAO figureHibernateDAO;
+
+	@Autowired
+	private ITetriminoDAO tetriminoHibernateDAO;
+
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+
+		int idFigure = Integer.parseInt(req.getParameter("id"));
+
+		Figure myFigure = figureHibernateDAO.find(idFigure);
+
+		Tetrimino myTetrimino = myFigure.getTetrimino();
+
+		int nelOrdre =  myFigure.getOrdre() + 1;
+
+		for (Figure figure : myTetrimino.getFigures()) {
+
+			if ( figure.getOrdre() == nelOrdre) {
+
+				figure.setOrdre(myFigure.getOrdre());
+
+				figureHibernateDAO.save(figure);
+			}
+
+			myFigure.setOrdre(nelOrdre);
+			figureHibernateDAO.save(myFigure);
+
+
+		}
+
+	}
+
+
+}
+
+
