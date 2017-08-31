0c665f979d50b84a8f2801257c3d8d5b295dca8d
Jeremy PERROUAULT <jeremy.perrouault@ascadis.fr>
2017-08-31 09:57:03 +0200

    Deconnexion de l'admin

--- src/com/sopra/controller/JoueurController.java
+++ src/com/sopra/controller/JoueurController.java
@@ -1,19 +1,13 @@
 package com.sopra.controller;
 
-import javax.validation.Valid;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.validation.BindingResult;
-import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.sopra.dao.IJoueurDAO;
-import com.sopra.model.Admin;
 import com.sopra.model.Joueur;
 
 @Controller
@@ -22,24 +16,22 @@ public class JoueurController {
 	@Autowired
 	private IJoueurDAO joueurDAO;
 
-	@RequestMapping(value="/joueurs", method=RequestMethod.GET)
+	@RequestMapping(value = "/joueurs", method = RequestMethod.GET)
 	public String getListeJoueurs(Model model) {
 		model.addAttribute("joueurs", joueurDAO.findAll());
 
 		return "joueurs";
 	}
-	
-	@RequestMapping(value="/joueur/bannir", method=RequestMethod.GET)
-	public String bannir(@RequestParam(value="id", required=true) int id) {
-		
-		
+
+	@RequestMapping(value = "/joueur/bannir", method = RequestMethod.GET)
+	public String bannir(@RequestParam(value = "id", required = true) int id) {
+
 		Joueur joueur = joueurDAO.find(id);
-		
+
 		joueur.setBanni(!joueur.getBanni());
-		
+
 		joueur = joueurDAO.save(joueur);
-	
+
 		return "redirect:/joueurs";
-		
-}
+	}
 }
