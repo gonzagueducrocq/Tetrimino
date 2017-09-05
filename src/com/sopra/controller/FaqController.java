eb2e0523155e7a6757e975d55c56767c3f2161e2
Jeremy PERROUAULT <jeremy.perrouault@ascadis.fr>
2017-09-05 16:06:30 +0200

    Modifs FAQ

--- src/com/sopra/controller/FaqController.java
+++ src/com/sopra/controller/FaqController.java
@@ -2,7 +2,12 @@ package com.sopra.controller;
 
 import java.util.List;
 
+import javax.annotation.PostConstruct;
+import javax.faces.context.FacesContext;
+import javax.servlet.http.HttpServletRequest;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Controller;
 
@@ -10,14 +15,25 @@ import com.sopra.dao.IFAQDAO;
 import com.sopra.model.FAQ;
 
 @Component
+@Scope("request")
 public class FaqController {
-
+	private int id;
 	private String question;
 	private String reponse;
 	
 	@Autowired
 	private IFAQDAO FAQDAO;
 	
+	
+	
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
 	public String getQuestion() {
 		return question;
 	}
@@ -33,25 +49,27 @@ public class FaqController {
 	public void setReponse(String reponse) {
 		this.reponse = reponse;
 	}
-
-	public String submit (int id) {
+	
+	
+	@PostConstruct
+	private void init() {
+		HttpServletRequest req = (HttpServletRequest)FacesContext.getCurrentInstance().getExternalContext().getRequest();
 		
-		FAQ faq = new FAQ();
-		
-		faq.setQuestion(getQuestion());
-		
-		faq.setReponse(getReponse());
-		
-		faq.setId(id);
-		
-		FAQDAO.save(faq);
-		
-		return "listeFAQ";
+		if (req.getParameter("id") != null) {
+			this.id = Integer.parseInt(req.getParameter("id"));
+			
+			FAQ faq = FAQDAO.find(this.id);
+			
+			this.question = faq.getQuestion();
+			this.reponse = faq.getReponse();
+		}
 	}
 	
 	public String submit() {
 		
 		FAQ faq = new FAQ();
+		
+		faq.setId(this.id);
 		
 		faq.setQuestion(getQuestion());
 		
